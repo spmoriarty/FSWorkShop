61b0adbab7bd044ee131f6e744aed6452933c0d3
Sean Moriarty <spmoriarty1@gmail.com>
2022-05-24 11:07:47 -0700

    fixed supabase pulled url/keys

--- fetch-utils.js
+++ fetch-utils.js
@@ -1,5 +1,5 @@
-const SUPABASE_URL = '';
-const SUPABASE_KEY = '';
+const SUPABASE_URL = 'https://xsjxkslcnzexkeuqopsq.supabase.co';
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inhzanhrc2xjbnpleGtldXFvcHNxIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NTIyOTI0MzIsImV4cCI6MTk2Nzg2ODQzMn0.6PW-I5jDnoqhgO6Egpvc5fEpprJ1jHfmAODRlCynp2I';
 
 const client = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 
